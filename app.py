fd639d927d5bd83890a11cfb982d9a39ac955646
haijin <haijin.zeng@gmail.com>
2024-10-19 06:47:43 +0000

    1

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ def index():
 
 @app.route("/prediciton_DBS",methods =["GET", 'POST'])
 def prediciton_DBS():
-    return(render_template("prediciton_DBS.html"))
-    #q = float(request.form.get("q"))
-    #return(render_template("prediciton_DBS.html" ,r=(-50.6*q)+90.2))
+    #return(render_template("prediciton_DBS.html"))
+    q = float(request.form.get("q"))
+    return(render_template("prediciton_DBS.html" ,r=(coef*q)+90.2))
 
 if __name__ == "__main__":
     app.run()
